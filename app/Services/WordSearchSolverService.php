1e74bcfb6f90b43ab65225ba19b79b0de0333053
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-12-02 13:24:08 -0500

    refactor: comment and improve readability of findWord() method

--- app/Services/WordSearchSolverService.php
+++ app/Services/WordSearchSolverService.php
@@ -2,83 +2,83 @@
 
 namespace App\Services;
 
-use Illuminate\Support\Facades\Log;
-
 class WordSearchSolverService {
-    public static function elementAdd(array $pos, array $direction) : array {
-        // Add direction to current position
-        return [
-            $pos[0] + $direction[0],
-            $pos[1] + $direction[1]
-        ];
-    }
-
+    
+    /**
+     * Determines if a word exists in a word search puzzle grid, if so,
+     * return the coordinates of all the matching characters
+     * @param string $word The word to find.
+     * @param array<string> $word_search The N by M word search grid.
+     * @return array The coordinates of the matching characters (row, column); otherwise empty array.
+     */
     public static function findWord(string $word, array $word_search) : array {
-        $arr_word = str_split(strtolower($word));
+        $word = strtolower($word);
+        $rows = count($word_search);
+        $cols = strlen($word_search[0]);
 
-        $UP = [-1, 0];
-        $DOWN = [1, 0];
-        $LEFT = [0, -1];
-        $RIGHT = [0, 1];
-        $TOPLEFT = [-1, -1];
-        $TOPRIGHT = [-1, 1];
-        $BOTTOMLEFT = [1, -1];
-        $BOTTOMRIGHT = [1, 1];        
-        $DIRS = [$UP, $DOWN, $LEFT, $RIGHT, $TOPLEFT, $TOPRIGHT, $BOTTOMLEFT, $BOTTOMRIGHT];
+        // Possible word checking directions
+        $DIRS = [
+            [-1, 0],  // UP
+            [1, 0],   // DOWN
+            [0, -1],  // LEFT
+            [0, 1],   // RIGHT
+            [-1, -1], // TOPLEFT
+            [-1, 1],  // TOPRIGHT
+            [1, -1],  // BOTTOMLEFT
+            [1, 1]    // BOTTOMRIGHT
+        ];
 
-        for ($r = 0; $r < count($word_search); $r++) {
-            for ($c = 0; $c < count($word_search); $c++) {
+        for ($r = 0; $r < $rows; $r++) {
+            for ($c = 0; $c < $cols; $c++) {
 
                 // Letter in array doesn't match with the initial letter of the word to find
-                if (strtolower($arr_word[0]) !== strtolower($word_search[$r][$c])) {
+                if ($word[0] !== strtolower($word_search[$r][$c])) {
                     continue;
                 }
 
                 $pos = [$r, $c];
-                foreach ($DIRS as &$DIR) {
+                foreach ($DIRS as $DIR) {
 
                     $matched_positions = [$pos]; // Positions that match chars
-                    $count = 1; // The count of matched chars
+                    $match_char_count = 1; // The count of matched chars
                     $cur_pos = $pos; // The state of the current position
 
                     while (true) {
-
-                        // The word was successfully found
-                        if ($count == strlen($word)) {
-                            return $matched_positions;
-                        }
-
-                        // Move the current position using the current direction
-                        $cur_pos = WordSearchSolverService::elementAdd($cur_pos, $DIR);
+                        // The new row and column after being shifted by the current direction
+                        $new_row = $cur_pos[0] + $DIR[0];
+                        $new_col = $cur_pos[1] + $DIR[1];
+                        $cur_pos = [$new_row, $new_col];
 
                         // In bounds
                         if (
-                            $cur_pos[0] >= 0 && $cur_pos[0] <= count($word_search) && 
-                            $cur_pos[1] >= 0 && $cur_pos[1] <= count($word_search)
+                            $new_row >= 0 && $new_row <= $rows && 
+                            $new_col >= 0 && $new_col <= $cols
                         ) {
-                            // Decompose x and y coordinate from current position
-                            $cur_pos_r = $cur_pos[0];
-                            $cur_pos_c = $cur_pos[1];
-                            
-                            // Get the characters from the word and word search for comparison
-                            $word_search_letter_at_pos = $word_search[$cur_pos_r][$cur_pos_c];
-                            $word_letter_at_pos = $word[$count];
+                            // Ensure grid letter is lowercase to prevent the equality check from failing
+                            $currentGridLetter = strtolower($word_search[$new_row][$new_col]);
+                            $currentWordLetter = $word[$match_char_count];
 
                             // If the chars match then add its position to the matched chars array
-                            if (strtolower($word_search_letter_at_pos) == strtolower($word_letter_at_pos)) {
-                                array_push($matched_positions, $cur_pos);
-                                $count++;
+                            if ($currentGridLetter == $currentWordLetter) {
+                                array_push($matched_positions, [$new_row, $new_col]);
+                                $match_char_count++;
                             } else {
                                 break;
                             }
                         } else {
                             break;
                         }
+
+                        // The word was successfully found
+                        if ($match_char_count == strlen($word)) {
+                            return $matched_positions;
+                        }
                     }
                 }
             }
         }
 
+        // Return an empty array if no match was found
         return [];
     }
 }
