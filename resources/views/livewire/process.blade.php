05bdbc261187581d337a1a32fa3cabcf3ed21a2c
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-12-03 20:55:15 -0500

    refactor: move process.css into process.blade.php and add active word category highlighting

--- resources/views/livewire/process.blade.php
+++ resources/views/livewire/process.blade.php
@@ -1,4 +1,37 @@
 <div>
+    <style>
+        .row-container {
+            margin-bottom: 1rem;
+        }
+
+        .row {
+            line-height: 24px;
+            font-size: 18px;
+            font-family: "Courier New", Courier, monospace;
+            display: block;
+        }
+
+        .word-container {
+            margin-bottom: 2.5rem;
+        }
+
+        .word {
+            display: block;
+            width: fit-content;
+            height: fit-content;
+            margin-bottom: 0.25rem;
+            cursor: pointer;
+        }
+
+        button {
+            margin-top: 1.5rem;
+        }
+
+        .letter.highlight {
+            color: red;
+        }
+    </style>
+    
     <h1>Word Search</h1>
     <div class="row-container">
         @foreach ($rows as $r=>&$row)
@@ -9,9 +42,15 @@
             </span>
         @endforeach
     </div>
-    <h2>Words: </h2>
-    @foreach ($word_array as &$word)
-        <span class="word" wire:click="modifyActiveWord('{{ $word }}')">{{ $word }}</span>
-    @endforeach
+    <h2>Words (Click to reveal): </h2>
+    <div class="word-container">
+        @foreach ($word_array as &$word)
+            <span 
+                class="word"
+                style="{{ $word == $active_word ? 'color: gray;' : '' }}"
+                wire:click="modifyActiveWord('{{ $word }}')">{{ $word }}
+            </span>
+        @endforeach
+    </div>
     <button onclick="window.location.href='{{ route('home') }}'">Return Home</button>
 </div>
