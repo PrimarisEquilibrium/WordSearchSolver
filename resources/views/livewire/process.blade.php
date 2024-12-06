4dcc71fe02cc24c939362cf45e2a8cbb7be75b20
Kevin <110409671+PrimarisEquilibrium@users.noreply.github.com>
2024-12-06 11:43:43 -0500

    feat: finish styling process page

--- resources/views/livewire/process.blade.php
+++ resources/views/livewire/process.blade.php
@@ -1,73 +1,44 @@
-<div>
-    <style>
-        .row-container {
-            margin-bottom: 1rem;
-        }
-
-        .row {
-            line-height: 24px;
-            font-size: 18px;
-            font-family: "Courier New", Courier, monospace;
-            display: block;
-        }
-
-        .word-container {
-            margin-bottom: 2.5rem;
-        }
-
-        .word {
-            display: block;
-            width: fit-content;
-            height: fit-content;
-            margin-bottom: 0.25rem;
-            cursor: pointer;
-        }
-
-        button {
-            margin-top: 1.5rem;
-        }
-
-        .letter.highlight {
-            color: red;
-        }
-    </style>
-    
-    <h1>Word Search</h1>
-    @if (!$edit_mode)
-        <div class="row-container">
-            @foreach ($rows as $r=>&$row)
-                <span class="row">
-                    @for ($c = 0; $c < strlen($row); $c++)
-                        <span class="letter {{ in_array([$r, $c], $highlighted_tiles) ? 'highlight' : '' }}">{{$row[$c]}}</span>
-                    @endfor
-                </span>
-            @endforeach
-        </div>
-    @else
-        <textarea 
-            name="new-board" 
-            id="new-board" 
-            cols="{{ strlen($rows[0]) * 1.5 }}" 
-            rows="{{ count($rows) }}"
-            wire:model="temp_rows"
-        >{{ implode(", \n", $rows) }}
-        </textarea>
-        <button wire:click="updateBoard">Confirm New Board</button>
-    @endif
-    
-    <br>
-    <button wire:click="toggleEditMode">Modify Board</button>
-    <br>
+<div class="prose">
+    <h1>Find Words</h1>
+    <div class="mb-4">
+        @if (!$edit_mode)
+            <div class="row-container font-mono	block">
+                @foreach ($rows as $r=>&$row)
+                    <span class="row block leading-6">
+                        @for ($c = 0; $c < strlen($row); $c++)
+                            <span class="letter {{ in_array([$r, $c], $highlighted_tiles) ? 'text-red-400' : '' }} mr-1">{{$row[$c]}}</span>
+                        @endfor
+                    </span>
+                @endforeach
+            </div>
+        @else
+            <textarea
+                name="new-board" 
+                id="new-board"
+                class="textarea textarea-bordered textarea-md w-full max-w-xs font-mono tracking-widest"
+                cols="{{ strlen($rows[0]) * 1.5 }}" 
+                rows="{{ count($rows) }}"
+                wire:model="temp_rows"
+            >{{ implode(", \n", $rows) }}</textarea>
+        @endif
+    </div>
+    <button class="btn btn-outline btn-info" wire:click="toggleEditMode">
+        @if (!$edit_mode)
+            Modify Board
+        @else
+            Confirm New Board
+        @endif
+    </button>
 
     <h2>Words (Click to reveal): </h2>
-    <div class="word-container">
+    <div class="word-container mb-8">
         @foreach ($word_array as &$word)
             <span 
-                class="word"
+                class="word text-lg"
                 style="{{ $word == $active_word ? 'color: gray;' : '' }}"
                 wire:click="modifyActiveWord('{{ $word }}')">{{ $word }}
             </span>
         @endforeach
     </div>
-    <button onclick="window.location.href='{{ route('home') }}'">Return Home</button>
+    <button class="btn btn-neutral btn-wide" onclick="window.location.href='{{ route('home') }}'">Return Home</button>
 </div>
